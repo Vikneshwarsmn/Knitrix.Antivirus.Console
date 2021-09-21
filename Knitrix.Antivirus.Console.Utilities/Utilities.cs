c69a03f4d2d929f3a7e7562c3b9ac0d7dc1d8fb1
Vikneshwar <s.vikneshwar@outlook.com>
2021-09-21 23:10:35 +0530

    Added a new Utility Project

---
+++ Knitrix.Antivirus.Console.Utilities/Utilities.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace Knitrix.Antivirus.Console.Utilities
+{
+    public class Utilities
+    {
+        private static readonly string QUARANTINE_PATH = "Quarantine";
+        public static string FileShortDescription(string fileName)
+        {
+            string fileShortDescription = fileName;
+            if (fileName.Length > 50)
+            {
+                string firstTenCharacters = fileName.Substring(0, 24);
+                string lastTenCharacters = fileName.Substring(fileName.Length - 24, 24);
+                fileShortDescription = firstTenCharacters + "......" + lastTenCharacters;
+            }
+            return fileShortDescription;
+        }
+
+        public static string GetFileSize(double bytes)
+        {
+            string size = "0 Bytes";
+            if (bytes >= 1073741824.0)
+                size = string.Format("{0:##.##}", bytes / 1073741824.0) + " GB";
+            else if (bytes >= 1048576.0)
+                size = string.Format("{0:##.##}", bytes / 1048576.0) + " MB";
+            else if (bytes >= 1024.0)
+                size = string.Format("{0:##.##}", bytes / 1024.0) + " KB";
+            else if (bytes > 0 && bytes < 1024.0)
+                size = bytes.ToString() + " Bytes";
+
+            return size;
+        }
+
+        public static string GetTimeElapsed(double milliSeconds)
+        {
+            string timeConsumed = "0 Seconds";
+            if (milliSeconds >= (24 * 60 * 60 * 1000))
+                timeConsumed = string.Format("{0:##.##}", milliSeconds / (24 * 60 * 60 * 1000)) + " Days";
+            else if (milliSeconds >= (60 * 60 * 1000))
+                timeConsumed = string.Format("{0:##.##}", milliSeconds / (60 * 60 * 1000)) + " Hours";
+            else if (milliSeconds >= (60 * 1000))
+                timeConsumed = string.Format("{0:##.##}", milliSeconds / (60 * 1000)) + " Minutes";
+            else if (milliSeconds > 0 && milliSeconds < (60 * 1000))
+                timeConsumed = string.Format("{0:##.##}", milliSeconds / 1000) + " Seconds";
+
+            return timeConsumed;
+        }
+
+        public static void QuarantineContainer(string scannedFile)
+        {
+            try
+            {
+                string quarantineFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, QUARANTINE_PATH);
+
+                if (!Directory.Exists(quarantineFolder))
+                    Directory.CreateDirectory(quarantineFolder);
+
+                string quarantinedFile = Path.Combine(quarantineFolder, Path.GetFileName(scannedFile));
+
+                if (!File.Exists(quarantinedFile))
+                    File.Copy(scannedFile, quarantinedFile);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
--- Knitrix.Antivirus.Console.nClam/Program.cs
+++ Knitrix.Antivirus.Console.nClam/Program.cs
@@ -3,12 +3,12 @@ using System.Linq;
 using nClam;
 using System.IO;
 using System.Diagnostics;
+using Knitrix.Antivirus.Console.Utilities;
 
 class Program
 {
     private static ClamClient CLAM_CLIENT;
     private static readonly string MALWARE_SAMPLES_PATH = @"E:\Projects\Clones\Malwares";
-    private static readonly string QUARANTINE_PATH = "Quarantine";
     private static long FILE_COUNT = 0;
     private static long CLEAN_FILES = 0;
     private static long INFECTED_FILES = 0;
@@ -44,8 +44,8 @@ class Program
         Console.WriteLine("Total Clean Files: " + CLEAN_FILES);
         Console.WriteLine("Total Infected Files: " + INFECTED_FILES);
         Console.WriteLine("Total Faulty Files: " + ERROR_FILES);
-        Console.WriteLine("Total Data Scanned: " + GetFileSize(TOTAL_SCAN_SIZE));
-        Console.WriteLine("Total Execution Time: " + GetTimeElapsed(watch.ElapsedMilliseconds));
+        Console.WriteLine("Total Data Scanned: " +  Utilities.GetFileSize(TOTAL_SCAN_SIZE));
+        Console.WriteLine("Total Execution Time: " + Utilities.GetTimeElapsed(watch.ElapsedMilliseconds));
 
         Console.ReadKey();
     }
@@ -84,7 +84,7 @@ class Program
             {
                 foreach (FileInfo fi in files)
                 {
-                    string fileShortDescription = FileShortDescription(fi.FullName);
+                    string fileShortDescription = Utilities.FileShortDescription(fi.FullName);
                     PrintLog("Current File: " + fileShortDescription);
                     ScanFile(fi.FullName);
                     Console.WriteLine(Environment.NewLine);
@@ -141,7 +141,7 @@ class Program
             case ClamScanResults.VirusDetected:
                 Console.WriteLine("Virus Found!");
                 Console.WriteLine("Virus name: {0}", scanResult.InfectedFiles.First().VirusName);
-                QuarantineContainer(fileToScan);
+                Utilities.QuarantineContainer(fileToScan);
                 INFECTED_FILES++;
                 break;
             case ClamScanResults.Error:
@@ -156,65 +156,5 @@ class Program
         Console.WriteLine($"{DateTime.Now.ToShortTimeString()}   {message}");
     }
 
-    private static string FileShortDescription(string fileName)
-    {
-        string fileShortDescription = fileName;
-        if (fileName.Length > 50)
-        {
-            string firstTenCharacters = fileName.Substring(0, 24);
-            string lastTenCharacters = fileName.Substring(fileName.Length - 24, 24);
-            fileShortDescription = firstTenCharacters + "......" + lastTenCharacters;
-        }
-        return fileShortDescription;
-    }
-
-    private static string GetFileSize(double bytes)
-    {
-        string size = "0 Bytes";
-        if (bytes >= 1073741824.0)
-            size = string.Format("{0:##.##}", bytes / 1073741824.0) + " GB";
-        else if (bytes >= 1048576.0)
-            size = string.Format("{0:##.##}", bytes / 1048576.0) + " MB";
-        else if (bytes >= 1024.0)
-            size = string.Format("{0:##.##}", bytes / 1024.0) + " KB";
-        else if (bytes > 0 && bytes < 1024.0)
-            size = bytes.ToString() + " Bytes";
-
-        return size;
-    }
-
-    private static string GetTimeElapsed(double milliSeconds)
-    {
-        string timeConsumed = "0 Seconds";
-        if (milliSeconds >= (24 * 60 * 60 * 1000))
-            timeConsumed = string.Format("{0:##.##}", milliSeconds / (24 * 60 * 60 * 1000)) + " Days";
-        else if (milliSeconds >= (60 * 60 * 1000))
-            timeConsumed = string.Format("{0:##.##}", milliSeconds / (60 * 60 * 1000)) + " Hours";
-        else if (milliSeconds >= (60 * 1000))
-            timeConsumed = string.Format("{0:##.##}", milliSeconds / (60 * 1000)) + " Minutes";
-        else if (milliSeconds > 0 && milliSeconds < (60 * 1000))
-            timeConsumed = string.Format("{0:##.##}", milliSeconds / 1000) + " Seconds";
-
-        return timeConsumed;
-    }
-
-    public static void QuarantineContainer(string scannedFile)
-    {
-        try
-        {
-            string quarantineFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, QUARANTINE_PATH);
-
-            if (!Directory.Exists(quarantineFolder))
-                Directory.CreateDirectory(quarantineFolder);
-
-            string quarantinedFile = Path.Combine(quarantineFolder, Path.GetFileName(scannedFile));
-
-            if (!File.Exists(quarantinedFile))
-                File.Copy(scannedFile, quarantinedFile);
-        }
-        catch (Exception ex)
-        {
-
-        }
-    }
+   
 }
