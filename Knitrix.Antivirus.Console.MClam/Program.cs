55de40c4418aa8eafd88a942bbf19727d9364cc1
Vikneshwar <s.vikneshwar@outlook.com>
2021-09-21 23:11:42 +0530

    Removed unused namespaces

--- Knitrix.Antivirus.Console.MClam/Program.cs
+++ Knitrix.Antivirus.Console.MClam/Program.cs
@@ -16,11 +16,6 @@
 //     along with this program; if not, write to the Free Software
 //     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using MClam;
 
 namespace FileScanExample
--- MClam.Sigtool/Properties/AssemblyInfo.cs
+++ MClam.Sigtool/Properties/AssemblyInfo.cs
@@ -16,7 +16,6 @@
 //     along with this program; if not, write to the Free Software
 //     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
